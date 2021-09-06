cd90c91cf152de5d5063ba70ca4f3e6f951f4141
alesukhin <alesukhin@museumplus.ru>
2021-09-06 23:08:54 +0300

    finish index page

--- src/js/main.js
+++ src/js/main.js
@@ -1,26 +1,51 @@
-import mapboxgl from 'mapbox-gl';
+import mapboxgl from "mapbox-gl";
 // import { doc } from 'prettier';
 
-document.addEventListener('DOMContentLoaded', () => {
+document.addEventListener("DOMContentLoaded", () => {
   // eslint-disable-next-line no-console
-  console.log('DOM полностью загружен и разобран');
-  require('./modules/main-nav');
-  let i;
+  console.log("DOM полностью загружен и разобран");
+  require("./modules/main-nav");
 
-  mapboxgl.accessToken =
-    'pk.eyJ1IjoiY2FuZWtsaXMiLCJhIjoiY2tqc2g2bWk1M3pyODJ6bG9jNTlicG1qbSJ9.kAq6U0hW3k2xL5j7paZWcg';
-  const map = new mapboxgl.Map({
-    container: 'poimapbox-map',
-    style: 'mapbox://styles/caneklis/ckq0szt1b090317mov88jqbuh',
-    center: [37.6136, 55.7328],
-    zoom: 8
+  const searchFormToggle = document.querySelector(".header__search-btn");
+  const searcForm = document.querySelector(".header__search");
+
+  searchFormToggle.addEventListener("click", () => {
+    let expanded = searchFormToggle.getAttribute("aria-expanded") === "true";
+    searchFormToggle.setAttribute("aria-expanded", !expanded);
+    searcForm.classList.add("header__search--open");
   });
 
-  const filters = document.querySelectorAll('.filter__item');
+  document.addEventListener("click", function (e) {
+    const target = e.target;
+    const itsMenu = target == searcForm || searcForm.contains(target);
+    // const its_btnMenu = target == btnMenu;
+    const menu_is_active = searcForm.classList.contains("header__search--open");
+
+    if (!itsMenu && menu_is_active) {
+      searcForm.classList.remove("header__search--open");
+    }
+  });
+
+  let i;
+
+  // mapboxgl.accessToken =
+  //   "pk.eyJ1IjoiY2FuZWtsaXMiLCJhIjoiY2tqc2g2bWk1M3pyODJ6bG9jNTlicG1qbSJ9.kAq6U0hW3k2xL5j7paZWcg";
+  mapboxgl.accessToken =
+    "pk.eyJ1IjoiYXJ0c3RvcnlsYWIiLCJhIjoiY2tzNGEzeDY5MGhtcDJ2cGhtYXJuY2t5ayJ9.2lmESFBMJsMghqwvNN4QMQ";
+  const map = new mapboxgl.Map({
+    container: "poimapbox-map",
+    // style: "mapbox://styles/caneklis/ckq0szt1b090317mov88jqbuh",
+    style: "mapbox://styles/artstorylab/cksgdswyk6isk17p1km65n92i",
+
+    center: [37.6136, 55.7328],
+    zoom: 3,
+  });
+
+  const filters = document.querySelectorAll(".filter__item");
 
   filters.forEach((filter) => {
-    filter.addEventListener('click', function () {
-      let selectedFilter = filter.getAttribute('data-filter');
+    filter.addEventListener("click", function () {
+      let selectedFilter = filter.getAttribute("data-filter");
       let itemsToHide = document.querySelectorAll(
         `.mapboxgl-marker:not([data-filter='${selectedFilter}'])`
       );
@@ -28,29 +53,29 @@ document.addEventListener('DOMContentLoaded', () => {
         `[data-filter='${selectedFilter}']`
       );
 
-      if (selectedFilter == 'all') {
+      if (selectedFilter == "all") {
         itemsToHide = [];
-        itemsToShow = document.querySelectorAll('[data-filter]');
+        itemsToShow = document.querySelectorAll("[data-filter]");
       }
 
       itemsToHide.forEach((el) => {
-        el.classList.add('hide');
-        el.classList.remove('show');
+        el.classList.add("hide");
+        el.classList.remove("show");
       });
 
       itemsToShow.forEach((el) => {
-        el.classList.remove('hide');
-        el.classList.add('show');
+        el.classList.remove("hide");
+        el.classList.add("show");
       });
     });
   });
 
   // This adds the data to the map
-  map.on('load', () => {
+  map.on("load", () => {
     // This is where your '.addLayer()' used to be, instead add only the source without styling a layer
-    map.addSource('places', {
-      type: 'geojson',
-      data: places
+    map.addSource("places", {
+      type: "geojson",
+      data: places,
     });
     // Initialize the list
     buildLocationList(places);
@@ -61,47 +86,47 @@ document.addEventListener('DOMContentLoaded', () => {
   // Now you have interactions with DOM markers instead
   places.features.forEach(function (marker, i) {
     // Create an img element for the marker
-    let el = document.createElement('div');
-    el.id = 'poimapbox-marker-' + i;
+    let el = document.createElement("div");
+    el.id = "poimapbox-marker-" + i;
     el.className = `poimapbox-marker  marker  ${marker.properties.type}`;
-    el.setAttribute('data-filter', `${marker.properties.type}`);
-    el.setAttribute('data-year', `${marker.properties.year}`);
+    el.setAttribute("data-filter", `${marker.properties.type}`);
+    el.setAttribute("data-year", `${marker.properties.year}`);
     // Add markers to the map at all points
     new mapboxgl.Marker(el, { offset: [0, 0] })
       .setLngLat(marker.geometry.coordinates)
       .addTo(map);
 
-    el.addEventListener('click', function (e) {
+    el.addEventListener("click", function (e) {
       // 1. Fly to the point
       flyToPark(marker);
 
       // 2. Close all other popups and display popup for clicked Park
       createPopUp(marker);
 
-      document.querySelector('.years').classList.add('years--hide');
+      document.querySelector(".years").classList.add("years--hide");
 
       // 3. Highlight listing in sidebar (and remove highlight for all other listings)
-      let activeItem = document.getElementsByClassName('active');
+      let activeItem = document.getElementsByClassName("active");
 
       e.stopPropagation();
       if (activeItem[0]) {
-        activeItem[0].classList.remove('active');
+        activeItem[0].classList.remove("active");
       }
 
-      let listing = document.getElementById('listing-' + i);
-      listing.classList.add('active');
+      let listing = document.getElementById("listing-" + i);
+      listing.classList.add("active");
     });
   });
 
   function flyToPark(currentFeature) {
     map.flyTo({
       center: currentFeature.geometry.coordinates,
-      zoom: 15
+      zoom: 15,
     });
   }
 
   function createPopUp(currentFeature) {
-    let popUps = document.getElementsByClassName('mapboxgl-popup');
+    let popUps = document.getElementsByClassName("mapboxgl-popup");
     if (popUps[0]) {
       popUps[0].remove();
     }
@@ -109,7 +134,7 @@ document.addEventListener('DOMContentLoaded', () => {
     let popupImg =
       currentFeature.properties.image != null
         ? `${currentFeature.properties.image}`
-        : '';
+        : "";
 
     let popup = new mapboxgl.Popup({ closeOnClick: false })
       .setLngLat(currentFeature.geometry.coordinates)
@@ -130,11 +155,11 @@ document.addEventListener('DOMContentLoaded', () => {
         `
       )
       .addTo(map);
-    popup.on('close', () => {
-      document.querySelector('.years').classList.remove('years--hide');
+    popup.on("close", () => {
+      document.querySelector(".years").classList.remove("years--hide");
     });
 
-    document.querySelector('.mapboxgl-popup-close-button').innerHTML = `
+    document.querySelector(".mapboxgl-popup-close-button").innerHTML = `
     <svg width="9" height="27" viewBox="0 0 9 27" fill="none" xmlns="http://www.w3.org/2000/svg">
       <path fill-rule="evenodd" clip-rule="evenodd" d="M8.37536 0.110007C8.10874 -0.0133388 7.79996 -0.0338504 7.51695 0.052984C7.23394 0.139818 6.99988 0.326886 6.86626 0.573038L0.119162 13.0313C0.040802 13.1756 0 13.3349 0 13.4964C0 13.6579 0.040802 13.8171 0.119162 13.9615L6.86626 26.4197C6.93172 26.5427 7.0229 26.6525 7.13453 26.7428C7.24617 26.833 7.37605 26.902 7.5167 26.9457C7.65735 26.9895 7.80599 27.007 7.95403 26.9975C8.10207 26.9879 8.2466 26.9514 8.37928 26.89C8.51196 26.8286 8.63017 26.7436 8.72709 26.6398C8.82401 26.5361 8.89772 26.4156 8.94398 26.2854C8.99024 26.1552 9.00812 26.0179 8.99661 25.8813C8.9851 25.7447 8.94441 25.6115 8.8769 25.4895L2.38394 13.4964L8.8769 1.50325C9.0105 1.25709 9.03272 0.972019 8.93866 0.710737C8.84461 0.449455 8.64198 0.233367 8.37536 0.110007Z" fill="#5896A3"/>
     </svg>
@@ -146,20 +171,21 @@ document.addEventListener('DOMContentLoaded', () => {
     for (i = 0; i < data.features.length; i++) {
       let currentFeature = data.features[i];
 
-      let listings = document.getElementById('poimapbox-listings');
-      let listing = listings.appendChild(document.createElement('li'));
-      listing.className = 'amenity-poi';
-      listing.id = 'listing-' + i;
+      let listings = document.getElementById("poimapbox-listings");
+      let listing = listings.appendChild(document.createElement("li"));
+      listing.className = "amenity-poi";
+      listing.id = "listing-" + i;
 
-      let link = listing.appendChild(document.createElement('a'));
-      link.href = '#';
-      link.className = 'name';
+      let link = listing.appendChild(document.createElement("a"));
+      link.href = "#";
+      link.className = "name";
       link.dataPosition = i;
       link.innerHTML = `
         <h3>${currentFeature.properties.name}</h3>
+        <p>${currentFeature.properties.subtitle}</p>
       `;
 
-      link.addEventListener('click', function (e) {
+      link.addEventListener("click", function (e) {
         // Update the currentFeature to the Park associated with the clicked link
         let clickedListing = data.features[this.dataPosition];
 
@@ -170,27 +196,29 @@ document.addEventListener('DOMContentLoaded', () => {
         createPopUp(clickedListing);
 
         // 3. Highlight listing in sidebar (and remove highlight for all other listings)
-        let activeItem = document.getElementsByClassName('amenity-poi active');
+        let activeItem = document.getElementsByClassName("amenity-poi active");
 
         if (activeItem[0]) {
-          activeItem[0].classList.remove('active');
+          activeItem[0].classList.remove("active");
         }
-        this.parentNode.classList.add('active');
+        this.parentNode.classList.add("active");
+
+        document.querySelector(".years").classList.add("years--hide");
       });
     }
   }
 
-  const filtersYears = document.querySelectorAll('.years__item');
+  const filtersYears = document.querySelectorAll(".years__item");
 
   filtersYears.forEach((filter) => {
-    filter.addEventListener('click', function () {
+    filter.addEventListener("click", function () {
       filtersYears.forEach((item) => {
-        item.classList.remove('years__item--active');
+        item.classList.remove("years__item--active");
       });
 
-      this.classList.add('years__item--active');
+      this.classList.add("years__item--active");
       // this.classList.add('years__item--active');
-      let selectedFilter = filter.getAttribute('data-year');
+      let selectedFilter = filter.getAttribute("data-year");
       let itemsToHide = document.querySelectorAll(
         `.mapboxgl-marker:not([data-year='${selectedFilter}'])`
       );
@@ -199,13 +227,13 @@ document.addEventListener('DOMContentLoaded', () => {
       );
 
       itemsToHide.forEach((el) => {
-        el.classList.add('hide');
-        el.classList.remove('show');
+        el.classList.add("hide");
+        el.classList.remove("show");
       });
 
       itemsToShow.forEach((el) => {
-        el.classList.remove('hide');
-        el.classList.add('show');
+        el.classList.remove("hide");
+        el.classList.add("show");
       });
     });
   });
@@ -215,64 +243,76 @@ document.addEventListener('DOMContentLoaded', () => {
   map.addControl(new mapboxgl.NavigationControl({ visualizePitch: true }));
 
   document
-    .querySelector('.mapboxgl-ctrl-zoom-in .mapboxgl-ctrl-icon')
+    .querySelector(".mapboxgl-ctrl-zoom-in .mapboxgl-ctrl-icon")
     .insertAdjacentHTML(
-      'afterbegin',
+      "afterbegin",
       `<svg width="30" height="30" viewBox="0 0 30 30" fill="none" xmlns="http://www.w3.org/2000/svg">
-    <path d="M15 2V28" stroke="#5896A3" stroke-width="3" stroke-linecap="square" stroke-linejoin="round"/>
-    <path d="M2 15L28 15" stroke="#5896A3" stroke-width="3" stroke-linecap="square" stroke-linejoin="round"/>
+    <path d="M15 2V28" stroke="currentColor" stroke-width="3" stroke-linecap="square" stroke-linejoin="round"/>
+    <path d="M2 15L28 15" stroke="currentColor" stroke-width="3" stroke-linecap="square" stroke-linejoin="round"/>
     </svg>`
     );
   document
-    .querySelector('.mapboxgl-ctrl-zoom-out .mapboxgl-ctrl-icon')
+    .querySelector(".mapboxgl-ctrl-zoom-out .mapboxgl-ctrl-icon")
     .insertAdjacentHTML(
-      'afterbegin',
+      "afterbegin",
       `<svg width="30" height="4" viewBox="0 0 30 4" fill="none" xmlns="http://www.w3.org/2000/svg">
-    <path d="M2 2L28 2" stroke="#5896A3" stroke-width="3" stroke-linecap="square" stroke-linejoin="round"/>
+    <path d="M2 2L28 2" stroke="currentColor" stroke-width="3" stroke-linecap="square" stroke-linejoin="round"/>
     </svg>
     `
     );
 
-  const filterList = document.querySelector('#menu');
+  const filterList = document.querySelector("#menu");
   document
-    .querySelector('.mapboxgl-ctrl-bottom-right')
-    .insertAdjacentElement('afterbegin', filterList);
+    .querySelector(".mapboxgl-ctrl-bottom-right")
+    .insertAdjacentElement("afterbegin", filterList);
 
-  const activateFilterBtn = document.querySelector('.filter__btn');
-  activateFilterBtn.addEventListener('click', () => {
-    activateFilterBtn.classList.toggle('filter__btn--active');
+  const activateFilterBtn = document.querySelector(".filter__btn");
+  activateFilterBtn.addEventListener("click", () => {
+    activateFilterBtn.classList.toggle("filter__btn--active");
     document
-      .querySelector('.filter__list')
-      .classList.toggle('filter__list--active');
+      .querySelector(".filter__list")
+      .classList.toggle("filter__list--active");
   });
 
-  const searchFormBtn = document.querySelector('.mapboxgl__search-form-toggle');
+  const searchFormBtn = document.querySelector(".mapboxgl__search-form-toggle");
 
-  searchFormBtn.addEventListener('click', () => {
+  searchFormBtn.addEventListener("click", () => {
     document
-      .querySelector('.poimapbox-sidebar')
-      .classList.toggle('poimapbox-sidebar--active');
-    searchFormBtn.classList.toggle('mapboxgl__search-form-toggle--active');
-    if (
-      searchFormBtn.classList.contains('mapboxgl__search-form-toggle--active')
-    ) {
-      searchFormBtn.innerHTML = `
-      <svg viewBox="0 0 32 32" xmlns="http://www.w3.org/2000/svg">
-        <use href="images/sprite_auto.svg#cross"/>
-      </svg>
-    `;
-    } else {
-      searchFormBtn.innerHTML = `
-      <svg viewBox="0 0 30 10" xmlns="http://www.w3.org/2000/svg">
-        <use href="images/sprite_auto.svg#search"/>
-      </svg>
-    `;
-    }
+      .querySelector(".poimapbox-sidebar")
+      .classList.add("poimapbox-sidebar--active");
+    searchFormBtn.style.display = "none";
+    // searchFormBtn.classList.toggle("mapboxgl__search-form-toggle--active");
+    // if (
+    //   searchFormBtn.classList.contains("mapboxgl__search-form-toggle--active")
+    // ) {
+    //   searchFormBtn.innerHTML = `
+    //   <svg viewBox="0 0 32 32" xmlns="http://www.w3.org/2000/svg">
+    //     <use href="images/sprite_auto.svg#cross"/>
+    //   </svg>
+    // `;
+    // } else {
+    //   searchFormBtn.innerHTML = `
+    //   <svg viewBox="0 0 30 10" xmlns="http://www.w3.org/2000/svg">
+    //     <use href="images/sprite_auto.svg#search"/>
+    //   </svg>
+    // `;
+    // }
+  });
+
+  const closeSidebarBtn = document.querySelector(
+    ".poimapbox-sidebar__close-btn"
+  );
+
+  closeSidebarBtn.addEventListener("click", () => {
+    document
+      .querySelector(".poimapbox-sidebar")
+      .classList.remove("poimapbox-sidebar--active");
+    searchFormBtn.style.display = "block";
   });
 
   document
-    .querySelector('#search-input')
-    .addEventListener('input', filterPlacesList);
+    .querySelector("#search-input")
+    .addEventListener("input", filterPlacesList);
   function filterPlacesList() {
     // Declare variables
     let input;
@@ -282,21 +322,21 @@ document.addEventListener('DOMContentLoaded', () => {
     let a;
     let i;
     let txtValue;
-    input = document.getElementById('search-input');
+    input = document.getElementById("search-input");
     filter = input.value.toUpperCase();
-    ul = document.getElementById('poimapbox-listings');
-    li = ul.getElementsByClassName('amenity-poi');
+    ul = document.getElementById("poimapbox-listings");
+    li = ul.getElementsByClassName("amenity-poi");
 
     // Loop through all list items, and hide those who don't match the search query
     for (i = 0; i < li.length; i++) {
-      a = li[i].getElementsByTagName('a')[0];
+      a = li[i].getElementsByTagName("a")[0];
       txtValue = a.textContent || a.innerText;
       if (txtValue.toUpperCase().indexOf(filter) > -1) {
-        li[i].style.display = '';
-        li[i].classList.remove('fadeOut');
+        li[i].style.display = "";
+        li[i].classList.remove("fadeOut");
       } else {
-        li[i].classList.add('fadeOut');
-        li[i].style.display = 'none';
+        li[i].classList.add("fadeOut");
+        li[i].style.display = "none";
       }
     }
   }
@@ -325,15 +365,15 @@ document.addEventListener('DOMContentLoaded', () => {
     this.resume();
   }
 
-  const playBtn = document.querySelector('.years-play-btn');
-  const stopBtn = document.querySelector('.years-stop-btn');
-  const resumeBtn = document.querySelector('.years-resume-btn');
-  playBtn.addEventListener('click', () => {
-    playBtn.classList.add('years-play-btn--hide');
-    stopBtn.classList.remove('years-stop-btn--hide');
+  const playBtn = document.querySelector(".years-play-btn");
+  const stopBtn = document.querySelector(".years-stop-btn");
+  const resumeBtn = document.querySelector(".years-resume-btn");
+  playBtn.addEventListener("click", () => {
+    playBtn.classList.add("years-play-btn--hide");
+    stopBtn.classList.remove("years-stop-btn--hide");
     let timer;
     timer = new RecurringTimer(function () {
-      let elems = document.querySelectorAll('.years__item');
+      let elems = document.querySelectorAll(".years__item");
 
       if (i <= elems.length) {
         index = index % elems.length;
@@ -343,16 +383,16 @@ document.addEventListener('DOMContentLoaded', () => {
 
     let index = 0;
 
-    stopBtn.addEventListener('click', () => {
+    stopBtn.addEventListener("click", () => {
       timer.pause();
-      stopBtn.classList.add('years-stop-btn--hide');
-      resumeBtn.classList.remove('years-resume-btn--hide');
+      stopBtn.classList.add("years-stop-btn--hide");
+      resumeBtn.classList.remove("years-resume-btn--hide");
       // timer = null;
     });
 
-    resumeBtn.addEventListener('click', () => {
-      stopBtn.classList.remove('years-stop-btn--hide');
-      resumeBtn.classList.add('years-resume-btn--hide');
+    resumeBtn.addEventListener("click", () => {
+      stopBtn.classList.remove("years-stop-btn--hide");
+      resumeBtn.classList.add("years-resume-btn--hide");
       timer.resume();
       // timer = null;
     });
